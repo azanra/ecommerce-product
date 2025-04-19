e1c4ea07909bae9d9ca4a58841e24e128724799d
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-19 15:37:16 +0700

    Initialize the product data

---
+++ scripts/model/data.js
@@ -0,0 +1,28 @@
+export default product = [
+  {
+    id: 0,
+    name: "Fall Limited Edition Sneakers",
+    description:
+      "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer",
+    originalPrice: 125,
+    Discount: "50%",
+    images: [
+      {
+        original: "./assets/images/image-product-1.jpg",
+        thumbnail: "./assets/images/image-product-1-thumbnail.jpg",
+      },
+      {
+        original: "./assets/images/image-product-2.jpg",
+        thumbnail: "./assets/images/image-product-2-thumbnail.jpg",
+      },
+      {
+        original: "./assets/images/image-product-3.jpg",
+        thumbnail: "./assets/images/image-product-3-thumbnail.jpg",
+      },
+      {
+        original: "./assets/images/image-product-3.jpg",
+        thumbnail: "./assets/images/image-product-3-thumbnail.jpg",
+      },
+    ],
+  },
+];
