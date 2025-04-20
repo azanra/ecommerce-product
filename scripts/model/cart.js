c0accd05e2ab189030aba2aff031d4d69217133f
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-20 13:36:59 +0700

    Add method to add or remove product from the cart

--- scripts/model/cart.js
+++ scripts/model/cart.js
@@ -6,4 +6,10 @@ export default class Cart {
   get cart() {
     return this.#cart;
   }
+  addToTheCart(item) {
+    this.#cart.push(item);
+  }
+  removeFromTheCart(itemId) {
+    this.#cart = this.#cart.filter((item) => item.id !== itemId);
+  }
 }
