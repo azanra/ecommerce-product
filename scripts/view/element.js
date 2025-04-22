b38124ae92d87a669788f8cbb2831a8e5ff8d929
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-22 09:11:14 +0700

    Set unique attribute to manipulate item from dom
    Use item id that retrieved from the id of an element
    to manipulate the cart and product, having each product
    dom having an unique id that used to identify them

--- scripts/view/element.js
+++ scripts/view/element.js
@@ -1,15 +1,24 @@
-class Element {
-  constructor(parentElement, element, attribute, textContent, uniqueId) {
+export default class Element {
+  constructor(parentElement, element, attribute, textContent) {
     this.parentElement = parentElement;
     this.element = element;
     this.attribute = attribute;
     this.textContent = textContent;
-    this.uniqueId = uniqueId;
   }
-  createElement() {
+  createElement(uniqueId) {
     const parentElement = document.querySelector(this.parentElement);
     const newElement = document.createElement(this.element);
     newElement.textContent = this.textContent;
+    this.setAttribute(newElement, uniqueId);
     parentElement.append(newElement);
+  }
+  setAttribute(newElement, uniqueId) {
+    for (const key in this.attribute) {
+      if (key === "id" && uniqueId !== undefined) {
+        newElement.setAttribute(key, `${this.attribute[key]}-${uniqueId}`);
+      } else {
+        newElement.setAttribute(key, this.attribute[key]);
+      }
+    }
   }
 }
