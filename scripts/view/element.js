00279ee96a8627474b2f83dbb976aa666d12a7d3
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-22 09:57:47 +0700

    Make the utility method as static instead

--- scripts/view/element.js
+++ scripts/view/element.js
@@ -1,29 +1,23 @@
 export default class Element {
-  constructor(parentElement, element, attribute, textContent) {
-    this.parentElement = parentElement;
-    this.element = element;
-    this.attribute = attribute;
-    this.textContent = textContent;
-  }
-  createElement(uniqueId) {
-    const parentElement = document.querySelector(this.parentElement);
-    const newElement = document.createElement(this.element);
-    this.setTextContent(newElement);
-    this.setAttribute(newElement, uniqueId);
+  static createElement(parent, element, attribute, textContent) {
+    const parentElement = document.querySelector(parent);
+    const newElement = document.createElement(element);
+    Element.setTextContent(newElement, textContent);
+    Element.setAttribute(newElement, attribute);
     parentElement.append(newElement);
   }
-  setAttribute(newElement, uniqueId) {
-    for (const key in this.attribute) {
+  static setAttribute(newElement, attribute) {
+    for (const key in attribute) {
       if (key === "id" && uniqueId !== undefined) {
-        newElement.setAttribute(key, `${this.attribute[key]}-${uniqueId}`);
+        newElement.setAttribute(key, `${attribute[key]}-${uniqueId}`);
       } else {
-        newElement.setAttribute(key, this.attribute[key]);
+        newElement.setAttribute(key, attribute[key]);
       }
     }
   }
-  setTextContent(element) {
-    if (this.textContent !== undefined) {
-      element.textContent = this.textContent;
+  static setTextContent(element, textContent) {
+    if (textContent !== undefined) {
+      element.textContent = textContent;
     }
   }
 }
